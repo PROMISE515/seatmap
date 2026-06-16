1e5b5593cdb882fb4b921101eb558edcd74800b1
BBAL2025 <sharefor129@gmail.com>
2026-06-16 16:52:37 +0800

    Keep old domain live until new domain is bound

--- src/lib/site.ts
+++ src/lib/site.ts
@@ -1,7 +1,5 @@
 export const SITE_URL = "https://westerntoiletmap.com";
 export const SITE_HOST = "westerntoiletmap.com";
 export const LEGACY_SITE_HOSTS = new Set([
-  "seatmapchina.com",
-  "www.seatmapchina.com",
   "www.westerntoiletmap.com",
 ]);
